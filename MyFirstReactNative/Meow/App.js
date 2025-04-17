c78060c645c81775d9a1495dc18659d653a2b1ef
Miranda Johnson <mj592@drexel.edu>
2025-04-16 21:50:29 -0400

    Assignment 1

---
+++ MyFirstReactNative/Meow/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello I am Miranda Johnson🎉</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f0f8ff',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
